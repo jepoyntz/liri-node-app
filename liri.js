b0d0650e7530d77f27aa0c2c665cdb840a5b6ff6
Gerald Poyntz <Gerald Poyntz>
2017-04-25 20:49:47 -0400

    changes

--- liri.js
+++ liri.js
@@ -1,34 +1,34 @@
 
 // Grabs the keys variables
-var keys = require("./keys.js"); 
-
-// Gets all of myKeys keys from the keys file.
-var keyList = keys.myKeys;
-console.log(keys)
+var keys = require("./keys"); 
+var request = require("request")
+// Gets all of keys from the keys file.
+var keyList = keys.twitterKeys;
+console.log(keyList)
 // Load the fs package to read and write
 var fs = require("fs");
 // Create a basic command line Node application using the inquirer package.
 var inquirer = require("inquirer");
-
+var Twitter = require('twitter'); //var Twitter = require('twitter')
 // Take one argument... set up for two 
 // The first will be the command (my-tweets,spotify-this-song,movie-this,do-what-it-says)   
 var command = process.argv[2];
 var extra = process.argv[3];
 
-inquirer.prompt([
+//inquirer.prompt([
     
-  {
-    type: "list",
-    message: "Welcome to LIRI...How can I help you?",
-    name: "doingWhat",
-    choices: ["my-tweets", "spotify-this-song", "movie-this","do-what-it-says"]
+  // {
+  //   type: "list",
+  //   message: "Welcome to LIRI...How can I help you?",
+  //   name: "doingWhat",
+  //   choices: ["my-tweets", "spotify-this-song", "movie-this","do-what-it-says"]
 
-  },    
+  //},    
   
   
-  ]);
+  //]);
 
-// We will then create a switch-case statement (if-then would also work).
+// create a switch-case statement 
 // The switch-case will direct which function gets run.
 switch (command) {
   case "my-tweets":
@@ -49,6 +49,25 @@ switch (command) {
 }
 
 function myTweets() {
+console.log("inside twitter function")
+
+
+var client = new Twitter(keyList);
+ 
+var params = {screen_name: 'gerald_poyntz'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    // tweets is an array
+    // tweets.forEach(function(tweet) {
+    //   console.log(tweet.text, tweet.created_at);
+    // });
+  }
+});
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+
 
 }
 
@@ -58,12 +77,15 @@ function spotify() {
 
 function movie() {
     // Then run a request to the OMDB API with the movie specified
-    var movieThis = "movie-this";
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&r=json";
+    if (!extra) {
+      extra = "Mr. Nobody";
+    }
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + extra + "&y=&plot=short&r=json";
 
     // This line is just to help us debug against the actual URL.
     console.log(queryUrl);
-
+  
    //standard request format
     request(queryUrl, function(error, response, body) {
 
@@ -79,11 +101,11 @@ function movie() {
     console.log("Language of the Movie: " + JSON.parse(body).Language);
     console.log("Plot of the Movie: " + JSON.parse(body).Plot);
     console.log("Actors in the Movie: " + JSON.parse(body).Actors);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings.Source[2].Value);
-    console.log("Rotten Tomatoes URL: " + JSON.parse("www.rottentomatoes.com");
+    console.log("Rotten Tomatoes Rating: " + JSON.stringify(JSON.parse(body).Ratings[1].Value));
+    console.log("Rotten Tomatoes URL: " + JSON.stringify(JSON.parse(body).Website));
   }
 
-  else 
+  else {
     var movieThis = "Mr_Nobody";
     var queryUrl = "http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&r=json";
     request(queryUrl, function(error, response, body) {
@@ -96,12 +118,13 @@ function movie() {
     console.log("Plot of the Movie: " + JSON.parse(body).Plot);
     console.log("Actors in the Movie: " + JSON.parse(body).Actors);
     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings.Source[2].Value);
-    console.log("Rotten Tomatoes URL: " + JSON.parse("www.rottentomatoes.com");
+    console.log("Rotten Tomatoes URL: " + JSON.parse("www.rottentomatoes.com"));
 });
 
 
 }
-
+})
+  }
 function doWhatItSays() {
 
 }
